fabec6ad48701e45b8d96ef00f17c4cceeb63e33
aidamzz <aidamohagheghzade@gmail.com>
2022-07-02 02:53:41 +0430

    binary tree

--- Binary_tree.py
+++ Binary_tree.py
@@ -7,3 +7,14 @@ class Node(object):
 class Binary_tree(object):
     def __init__(self, root):
         self.root = Node(root)
+    def print_tree(self, traversal_type):
+        if traversal_type == 'preorder':
+            return self.preorder_print(tree.root, "")
+        else:
+            print("Traversal type "+ str(traversal_type)+ " is not supported.")
+    def preorder_print(self, start, traversal):
+        if start:
+            traversal += (str(start.value)+ "-")
+            traversal = self.preorder_print(start.left, traversal)
+            traversal = self.preorder_print(start.right, traversal)
+        return traversal
