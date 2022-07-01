ec2e8603521e3ed2eab72c304945d066e274417d
aidamzz <aidamohagheghzade@gmail.com>
2022-07-02 04:00:23 +0430

    inorder postorder

--- Binary_tree.py
+++ Binary_tree.py
@@ -10,6 +10,10 @@ class Binary_tree(object):
     def print_tree(self, traversal_type):
         if traversal_type == 'preorder':
             return self.preorder_print(tree.root, "")
+        elif traversal_type == 'inorder':
+            return self.inorder_print(tree.root, "")
+        elif traversal_type == 'postorder':
+            return self.postorder_print(tree.root, "")
         else:
             print("Traversal type "+ str(traversal_type)+ " is not supported.")
     def preorder_print(self, start, traversal):
@@ -17,6 +21,18 @@ class Binary_tree(object):
             traversal += (str(start.value)+ "-")
             traversal = self.preorder_print(start.left, traversal)
             traversal = self.preorder_print(start.right, traversal)
+        return traversal
+    def inorder_print(self, start, traversal):
+        if start:
+            traversal = self.inorder_print(start.left, traversal)
+            traversal += (str(start.value)+'-')
+            traversal = self.inorder_print(start.right, traversal)
+        return traversal
+    def postorder_print(self, start, traversal):
+        if start:
+            traversal = self.inorder_print(start.left, traversal)
+            traversal = self.inorder_print(start.right, traversal)
+            traversal += (str(start.value)+ "-")
         return traversal
 
 # %%
