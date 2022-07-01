7f9d18748311be83ef4f08c174486d910d2e2242
aidamzz <aidamohagheghzade@gmail.com>
2022-07-02 02:55:05 +0430

    binary tree

--- Binary_tree.py
+++ Binary_tree.py
@@ -18,3 +18,17 @@ class Binary_tree(object):
             traversal = self.preorder_print(start.left, traversal)
             traversal = self.preorder_print(start.right, traversal)
         return traversal
+
+# %%
+'''tree = Binary_tree(1)
+tree.root.left = Node(2)
+tree.root.right = Node(3)
+tree.root.left.left = Node(4)
+tree.root.left.right = Node(5)
+tree.root.right.left = Node(6)
+tree.root.right.right = Node(7)
+tree.root.right.right.right = Node(8)'''
+
+'''# %%
+print(tree.print_tree('preorder'))
+1-2-4-5-3-6-7-8-'''
