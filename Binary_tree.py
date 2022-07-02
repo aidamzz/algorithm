f3d2a717cafeb2269fb8e1e841080705b49ed18c
aidamzz <aidamohagheghzade@gmail.com>
2022-07-02 19:15:29 +0430

    level order

--- Binary_tree.py
+++ Binary_tree.py
@@ -1,3 +1,28 @@
+class Queue(object):
+    def __init__(self):
+        self.items = []
+    
+    def enqueue(self, item):
+        self.items.insert(0,item)
+    
+    def dequeue(self):
+        if not self.is_empty():
+            return self.items.pop()
+    
+    def is_empty(self):
+        return len(self.items) == 0
+    
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1].value
+    
+    def __len__(self):
+        return self.size()
+    
+    def size(self):
+        return len(self.items)
+    
+
 class Node(object):
     def __init__(self,value):
         self.value = value
@@ -7,6 +32,7 @@ class Node(object):
 class Binary_tree(object):
     def __init__(self, root):
         self.root = Node(root)
+    
     def print_tree(self, traversal_type):
         if traversal_type == 'preorder':
             return self.preorder_print(tree.root, "")
@@ -14,25 +40,48 @@ class Binary_tree(object):
             return self.inorder_print(tree.root, "")
         elif traversal_type == 'postorder':
             return self.postorder_print(tree.root, "")
+        elif traversal_type == "levelorder":
+            return self.levelorder_print(tree.root)
         else:
             print("Traversal type "+ str(traversal_type)+ " is not supported.")
+    
     def preorder_print(self, start, traversal):
         if start:
             traversal += (str(start.value)+ "-")
             traversal = self.preorder_print(start.left, traversal)
             traversal = self.preorder_print(start.right, traversal)
         return traversal
+    
     def inorder_print(self, start, traversal):
         if start:
             traversal = self.inorder_print(start.left, traversal)
             traversal += (str(start.value)+'-')
             traversal = self.inorder_print(start.right, traversal)
         return traversal
+    
     def postorder_print(self, start, traversal):
         if start:
             traversal = self.inorder_print(start.left, traversal)
             traversal = self.inorder_print(start.right, traversal)
             traversal += (str(start.value)+ "-")
+        return traversal
+    
+    def levelorder_print(self, start):
+        if start is None:
+            return
+        
+        queue = Queue()
+        queue.enqueue(start)
+
+        traversal = ""
+        while len(queue)>0:
+            traversal += str(queue.peek())+ '-'
+            node = queue.dequeue()
+
+            if node.left:
+                queue.enqueue(node.left)
+            if node.right:
+                queue.enqueue(node.right)
         return traversal
 
 # %%
@@ -47,4 +96,7 @@ tree.root.right.right.right = Node(8)'''
 
 '''# %%
 print(tree.print_tree('preorder'))
-1-2-4-5-3-6-7-8-'''
+1-2-4-5-3-6-7-8-
+# %%
+print(tree.print_tree('levelorder'))
+1-2-3-4-5-'''
