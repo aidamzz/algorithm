9c42bf65d6f1dfeeec035194411096dec68a3e3a
aidamzz <aidamohagheghzade@gmail.com>
2022-07-02 18:50:03 +0430

    palindrome

---
+++ Palindrome.py
@@ -0,0 +1,14 @@
+def is_palindrome(s):
+    i = 0
+    j = len(s)-1
+
+    while i <j:
+        while not s[i].isalnum() and i <j:
+            i += 1
+        while not s[j].isalnum() and i<j:
+            j -= 1
+        if s[i].lower() != s[j].lower():
+            return False
+        i +=1
+        j -=1
+    return True
