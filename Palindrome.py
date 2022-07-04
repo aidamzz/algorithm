58c9f4c89dbaf95af47299b3e76082728bb33dba
aidamzz <aidamohagheghzade@gmail.com>
2022-07-04 13:18:45 +0430

    fix

--- Palindrome.py
+++ Palindrome.py
@@ -2,13 +2,13 @@ def is_palindrome(s):
     i = 0
     j = len(s)-1
 
-    while i <j:
-        while not s[i].isalnum() and i <j:
+    while i < j:
+        while not s[i].isalnum() and i < j:
             i += 1
-        while not s[j].isalnum() and i<j:
+        while not s[j].isalnum() and i < j:
             j -= 1
         if s[i].lower() != s[j].lower():
             return False
-        i +=1
-        j -=1
+        i += 1
+        j -= 1
     return True
