788089ff41d489d3fafeca325cafb8028c350812
aida mz <aidamohagheghzade@gmail.com>
2024-04-21 22:17:24 +0330

    fix problems

--- HeapSort/heapSort.py
+++ HeapSort/heapSort.py
@@ -1,22 +1,21 @@
 def heapify(arr, n, i):
-    # find largest among root and children
     largest = i
-    l = 2*i +1
-    r = 2*i + 2
-    if l< n and arr[i] < arr[l]:
+    l = 2 * i + 1
+    r = 2 * i + 2
+    if l < n and arr[i] < arr[l]:
         largest = l
-    if r < n and arr[i] < arr[r]:
+    if r < n and arr[largest] < arr[r]:
         largest = r
     if largest != i:
-        arr[largest], arr[i] = arr[i], arr[largest]
+        arr[i], arr[largest] = arr[largest], arr[i]
         heapify(arr, n, largest)
-        print(arr)
 
 def heapSort(arr):
     n = len(arr)
     for i in range(n//2, -1, -1):
         heapify(arr, n, i)
-    for i in range(n-1, 0,-1):
-        #swap
-        arr[i], arr[0] = arr[0], arr[1]
-        heapify(arr, i , 0)
+    for i in range(n-1, 0, -1):
+        # Swap
+        arr[i], arr[0] = arr[0], arr[i]
+        # Heapify root element
+        heapify(arr, i, 0)
